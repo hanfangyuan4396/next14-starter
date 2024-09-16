611f0bb719ed8034e878741ea434bef4590afd65
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-09-16 21:44:24 +0800

    contact and blog page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+      remotePatterns:[
+        {
+          protocol: 'https',
+          hostname: "c-ssl.dtstatic.com"
+        }
+      ]
+    }
+  }
+  
 
 export default nextConfig;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,27 @@
+import styles from "./contact.module.css"
+
 const ContactPage = () => {
     return (
-        <div>ContactPage</div>
+        <div className={styles.container}>
+          <div className={styles.imgContainer}>
+            <img src="contact.png" fill />
+          </div>
+          <div className={styles.formContainer}>
+            <form action="" className={styles.form}>
+              <input type="text" placeholder="Name and Surname" />
+              <input type="text" placeholder="Email Address" />
+              <input type="text" placeholder="Phone Number (Optional)" />
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="Message"
+              />
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
     )
 };
 
